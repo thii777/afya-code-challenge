cf9f1a7f6880216c8f65587e64e6840e2dd25373
thiago carvalho <thiagocarvalhogalvao@gmail.com>
2023-02-24 02:01:14 -0300

    infra: config class-validator, swagger and add some libs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { SET_DESCRIPTION_SWAGGER, SET_TITLE_SWAGGER } from './constant';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle(SET_TITLE_SWAGGER)
+    .setDescription(SET_DESCRIPTION_SWAGGER)
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/documentation', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
