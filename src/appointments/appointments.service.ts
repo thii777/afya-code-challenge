9545566ae7c7458aaaba221a8ccf93a2720ead27
thiago carvalho <thiagocarvalhogalvao@gmail.com>
2023-02-26 23:01:20 -0300

    Revert "feat: add checkAppointmentAvailability function for appointment availability checking"
    This reverts commit 9b8325fb60463cf6ead0f83ac496f558f14b02b0.

--- src/appointments/appointments.service.ts
+++ src/appointments/appointments.service.ts
@@ -1,29 +1,51 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Patient } from '../patients/entities/patient.entity';
-import { Repository } from 'typeorm';
+import { Between, Repository } from 'typeorm';
 import { CreateAppointmentDto } from '../appointments/dto/create-appointment.dto';
 import { UpdateAppointmentDto } from '../appointments/dto/update-appointment.dto';
 import { Appointment } from '../appointments/entities/appointment.entity';
-import { checkAppointmentAvailability } from './appointment.helper';
 import {
+  INVALID_DATE,
   PATIENT_NOT_FOUND,
-  TIME_NOT_AVAILABLE,
 } from '../appointments/errors.constants';
 
-const MINIMUM_INTERVAL_DURATION_SECONDS = 3540;
+const MINIMUM_INTERVAL_DURATION_SECONDS = 3600;
+
 @Injectable()
 export class AppointmentsService {
   constructor(
     @InjectRepository(Appointment)
-    private appointmentRepo: Repository<Appointment>,
+    private appointmentRepository: Repository<Appointment>,
     @InjectRepository(Patient)
-    private patientRepo: Repository<Patient>,
+    private patientRepository: Repository<Patient>,
   ) {}
+
+  async verificarDisponibilidade(startDateTime: Date): Promise<boolean> {
+    startDateTime = new Date(startDateTime);
+    startDateTime.setHours(startDateTime.getHours() + 3);
+
+    const now = new Date();
+
+    if (
+      startDateTime.getTime() <
+      now.getTime() + MINIMUM_INTERVAL_DURATION_SECONDS * 1000
+    ) {
+      throw new HttpException(INVALID_DATE, HttpStatus.BAD_REQUEST);
+    }
+
+    const oneHourLater = new Date(startDateTime.getTime() + 60 * 60 * 1000);
+    const agendamentos = await this.appointmentRepository.findOne({
+      where: {
+        startDateTime: Between(startDateTime, oneHourLater),
+      },
+    });
+    if (!agendamentos) return true;
+  }
 
   async create(createAppointmentDto: CreateAppointmentDto) {
     try {
-      const patient = await this.patientRepo.findOne({
+      const patient = await this.patientRepository.findOne({
         where: { document: createAppointmentDto.document },
       });
 
@@ -35,48 +57,36 @@ export class AppointmentsService {
         throw error;
       }
 
-      const startDateTime = new Date(createAppointmentDto.startDateTime);
-
+      console.log(createAppointmentDto, '1');
       const appointment = new Appointment();
       Object.assign(appointment, createAppointmentDto);
       appointment.patientId = patient.id;
 
-      const endDateTime = new Date(
-        startDateTime.getTime() + MINIMUM_INTERVAL_DURATION_SECONDS * 1000,
-      );
-      appointment.endDateTime = endDateTime;
+      // const disponivel = await this.verificarDisponibilidade(
+      //   appointment.startDateTime,
+      // );
 
-      const isAvailable = await checkAppointmentAvailability(
-        this.appointmentRepo,
-        appointment.startDateTime,
-      );
-
-      if (!isAvailable) {
-        const error = new HttpException(
-          TIME_NOT_AVAILABLE,
-          HttpStatus.CONFLICT,
-        );
-        throw error;
-      }
-
-      return await this.appointmentRepo.save(appointment);
+      // if (!disponivel) {
+      //   throw new Error('O horário selecionado não está disponível');
+      // }
+      console.log(appointment, 'appointment');
+      return await this.appointmentRepository.save(appointment);
     } catch (error) {
-      if (error.message.includes('External service unavailable')) {
-        return Promise.reject(error);
-      }
+      console.log(error, 'error');
       return error;
     }
   }
 
   async findAll(): Promise<Appointment[]> {
-    return await this.appointmentRepo.find();
+    return await this.appointmentRepository.find();
   }
 
   async update(id: any, _updateAppointmentDto: UpdateAppointmentDto) {
-    return await this.appointmentRepo.update(id, _updateAppointmentDto);
+    console.log(id, _updateAppointmentDto, 'update appoint');
+    return await this.appointmentRepository.update(id, _updateAppointmentDto);
   }
 
   async remove(id: any) {
-    return await this.appointmentRepo.delete(id);
+    return await this.appointmentRepository.delete(id);
   }
 }
